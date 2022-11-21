768e260cd99dc26d4a66db6e421f0d6858036a1f
surendra <surendra.singh@eukleiatech.com>
2022-11-21 11:07:02 +0530

    admin panel crud operation

---
+++ vendor/encore/laravel-admin/resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        
+    </div>
+    <!-- Default to the left -->
+    <strong>Powered by <a href="#" target="_blank">Surendra Singh</a></strong>
+</footer>
---
+++ vendor/encore/laravel-admin/resources/views/partials/sidebar.blade.php
@@ -0,0 +1,34 @@
+<aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ Admin::user()->avatar }}" class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+                <p>{{ Admin::user()->name }}</p>
+                <!-- Status -->
+                <a href="#"><i class="fa fa-circle text-success"></i> {{ trans('admin.online') }}</a>
+            </div>
+        </div>
+
+        @if(config('admin.enable_menu_search'))
+        <!-- search form (Optional) -->
+        
+        <!-- /.search form -->
+        @endif
+
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu">
+            <li class="header">{{ trans('admin.menu') }}</li>
+
+            @each('admin::partials.menu', Admin::menu(), 'item')
+
+        </ul>
+        <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
